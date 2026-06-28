883ba78a8c20d4296f14b34c5bd93bc4a444ba24
Diane Doan <dianeddoan@gmail.com>
2026-06-28 12:23:26 -0600

    Add fibsRec recursive function

--- fibonacci.js
+++ fibonacci.js
@@ -14,3 +14,21 @@ console.log(fibs(1));
 console.log(fibs(2));
 console.log(fibs(3));
 console.log(fibs(4));
+
+function fibsRec(num) {
+  // base cases
+  if (num === 0) return [0];
+  if (num === 1) return [0, 1];
+
+  // recursive case
+  const sequence = fibsRec(num - 1);
+  sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+
+  return sequence;
+}
+
+console.log(fibsRec(0));
+console.log(fibsRec(1));
+console.log(fibsRec(2));
+console.log(fibsRec(3));
+console.log(fibsRec(4));
